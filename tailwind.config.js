4b0bd9cf6476796ff9f5da28a87f15b7818fd505
Tom Gallagher <tgallagher182@gmail.com>
2023-05-06 13:48:12 -0500

    test commit ability

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,27 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+
+      white: '#fff',
+      gray: {
+        50: '#f9fafb',
+        100: '#f4f5f7',
+        200: '#e5e7eb',
+
+        300: '#d2d6dc',
+        400: '#9fa6b2',
+        500: '#6b7280',
+
+        600: '#4b5563',
+        700: '#374151',
+        800: '#252f3f',
+
+      }
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
