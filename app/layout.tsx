45c71eec01cda448dd79c2c218589c065f25f3ce
Tom Gallagher <tgallagher182@gmail.com>
2023-05-07 20:51:15 -0500

    fucking stupid

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Sidebar from './component/sidebar'
+import Sidebar from './component/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
