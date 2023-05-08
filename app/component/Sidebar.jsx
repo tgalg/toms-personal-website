789c7cadcda62c01b8681dab0b98973699a38a45
Tom Gallagher <tgallagher182@gmail.com>
2023-05-07 19:53:12 -0500

    get a clean template working

--- app/component/Sidebar.jsx
+++ app/component/Sidebar.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+import { Envelope, Twitter, Linkedin, Medium } from "./icons/";
 
-export default Sidebar
+const Sidebar = ({ data }) => {
+	const { name, role, education, contactLinks } = data;
+
+	return (
+		<div className='bg-black flex flex-col  content-between w-full h-auto sm:h-screen sm:justify-around sm:w-1/3 sm:fixed'>
+			<div className='text-white flex flex-col p-10 items-center'>
+				<Image
+					priority
+					width={300}
+					height={300}
+					className='rounded-full h-full mb-6'
+					src='/images/tom_photo.jpg'
+					alt='tom_photo.jpg'
+					aria-label='tom_photo.jpg'
+				/>
+				<h1 className='mb-2'>{name}</h1>
+				<h2 className='mb-8'>{role}</h2>
+				<p className='mb-2'>{education[0]}</p>
+				<p className='mb-2'>{education[1]}</p>
+				<div className='text-white text-center mb-4 mt-4 sm:mt-8'>
+					<h3 className='mb-2'> CONTACT ME</h3>
+					<div className='flex flex-row justify-center gap-2'>
+						<a
+							className='icons-contactme'
+							href={contactLinks?.[0]}
+							aria-label={"email link"}
+						>
+							<Envelope />
+						</a>
+						<a
+							className='icons-contactme'
+							href={contactLinks?.[1]}
+							aria-label={"twitter link"}
+						>
+							<Twitter />
+						</a>
+						<a
+							className='icons-contactme'
+							href={contactLinks?.[2]}
+							aria-label={"linkedin link"}
+						>
+							<Linkedin />
+						</a>
+						<a
+							className='icons-contactme'
+							href={contactLinks?.[3]}
+							aria-label={"medium link"}
+						>
+							<Medium />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
