34dc3483b35fd8073dcb675d12338e70df448322
Tom Gallagher <tgallagher182@gmail.com>
2023-05-09 13:18:28 -0500

    adds prettier chagnes

--- app/component/skills.jsx
+++ app/component/skills.jsx
@@ -1,46 +1,45 @@
-"use client";
-import React, { useState } from "react";
+'use client'
+import React, { useState } from 'react'
 
 const Skills = ({ data }) => {
-	const [activeTab, setActiveTab] = useState("hard");
+  const [activeTab, setActiveTab] = useState('hard')
 
-	const setBg = (active) => (activeTab === active ? "bg-yellow" : "bg-grey");
-	const setTabsAlignment = (tab) =>
-		tab === "soft" ? "text-right" : "text-left";
+  const setBg = active => (activeTab === active ? 'bg-yellow' : 'bg-grey')
+  const setTabsAlignment = tab => (tab === 'soft' ? 'text-right' : 'text-left')
 
-	const tabs = (
-		<div className='flex'>
-			{["hard", "soft"].map((el) => (
-				<button
-					key={el}
-					type='button'
-					className={`btn ${setBg(el)} ${setTabsAlignment(el)}`}
-					onClick={() => setActiveTab(el)}
-				>
-					{el} Skills
-				</button>
-			))}
-		</div>
-	);
-	const content = (
-		<ul
-			className={`flex flex-row flex-wrap content-start list-none py-4 gap-2 ${
-				activeTab === "soft" ? "justify-end" : "justify-start"
-			}`}
-		>
-			{data[activeTab].map(({ icon, text }) => (
-				<li key={text} className='skill'>
-					<span> {icon}</span> {text}
-				</li>
-			))}
-		</ul>
-	);
-	return (
-		<div>
-			{tabs}
-			{content}
-		</div>
-	);
-};
+  const tabs = (
+    <div className="flex">
+      {['hard', 'soft'].map(el => (
+        <button
+          key={el}
+          type="button"
+          className={`btn ${setBg(el)} ${setTabsAlignment(el)}`}
+          onClick={() => setActiveTab(el)}
+        >
+          {el} Skills
+        </button>
+      ))}
+    </div>
+  )
+  const content = (
+    <ul
+      className={`flex flex-row flex-wrap content-start list-none py-4 gap-2 ${
+        activeTab === 'soft' ? 'justify-end' : 'justify-start'
+      }`}
+    >
+      {data[activeTab].map(({ icon, text }) => (
+        <li key={text} className="skill">
+          <span> {icon}</span> {text}
+        </li>
+      ))}
+    </ul>
+  )
+  return (
+    <div>
+      {tabs}
+      {content}
+    </div>
+  )
+}
 
-export default Skills;
+export default Skills
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,44 @@
-import "./globals.css";
+import './globals.css'
 import Head from 'next/head'
 
-import Sidebar from "./component/sidebar"
-import { personalData } from "../data/page-data"
-import next from "next/types";
+import Sidebar from './component/sidebar'
+import { personalData } from '../data/page-data'
+import next from 'next/types'
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode
 }) {
-	return (
-		<html>
-			<head>
-				<Head>
-					<title>My Portfolio</title>
-					<meta name='description' content='My Portfolio' />
-				</Head>
-			</head>
+  return (
+    <html>
+      <head>
+        <Head>
+          <title>My App Title</title>
+          <meta name="description" content="My App Description" />
+          <meta property="og:title" content="My App Title" />
+          <meta property="og:description" content="My App Description" />
+          <meta
+            property="og:image"
+            content="https://example.com/my-image.jpg"
+          />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:title" content="My App Title" />
+          <meta name="twitter:description" content="My App Description" />
+          <meta
+            name="twitter:image"
+            content="https://example.com/my-image.jpg"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+      </head>
 
-			<body className='flex flex-col sm:flex-row'>
-				<Sidebar data={personalData} />
-				<main className='grow-full p-8 sm:p-16 w-full sm:basis-2/3  ml-auto'>
-					{children}
-				</main>
-			</body>
-		</html>
-	);
+      <body className="flex flex-col sm:flex-row">
+        <Sidebar data={personalData} />
+        <main className="grow-full p-8 sm:p-16 w-full sm:basis-2/3  ml-auto">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
 }
