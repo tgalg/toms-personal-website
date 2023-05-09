e12e374dc0489e1c34e86ad792a2ec3d6760a0cb
Tom Gallagher <tgallagher182@gmail.com>
2023-05-09 13:47:29 -0500

    use metadata over head

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,10 @@ import React from 'react'
 import AboutMe from './component/about-me'
 import { aboutMe, skills, professionalData } from '../data/page-data'
 import ProfessionalExperience from './component/professional-experience'
-import Head from 'next/head'
 
 const page = () => {
   return (
     <div>
-      <Head>
-        <title>My App Title</title>
-        <meta name="description" content="My App Description" />
-        <meta property="og:title" content="My App Title" />
-        <meta property="og:description" content="My App Description" />
-        <meta property="og:image" content="https://example.com/my-image.jpg" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:title" content="My App Title" />
-        <meta name="twitter:description" content="My App Description" />
-        <meta name="twitter:image" content="https://example.com/my-image.jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <AboutMe data={aboutMe} skills={skills} />
       <ProfessionalExperience data={professionalData} />
     </div>
