fb3879c4b2b0d93b314800cf83745a72cdbfe0eb
Tom Gallagher <tgallagher182@gmail.com>
2023-05-09 13:30:37 -0500

    adds prettier chagnes 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import './globals.css'
-import Head from 'next/head'
 
 import Sidebar from './component/sidebar'
 import { personalData } from '../data/page-data'
-import next from 'next/types'
 
 export default function RootLayout({
   children,
@@ -12,26 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
-        <Head>
-          <title>My App Title</title>
-          <meta name="description" content="My App Description" />
-          <meta property="og:title" content="My App Title" />
-          <meta property="og:description" content="My App Description" />
-          <meta
-            property="og:image"
-            content="https://example.com/my-image.jpg"
-          />
-          <meta property="og:type" content="website" />
-          <meta name="twitter:title" content="My App Title" />
-          <meta name="twitter:description" content="My App Description" />
-          <meta
-            name="twitter:image"
-            content="https://example.com/my-image.jpg"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-      </head>
+      <head></head>
 
       <body className="flex flex-col sm:flex-row">
         <Sidebar data={personalData} />
